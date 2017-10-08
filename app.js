5e710470e5ff2f5d4526470779e61691568be875
marcpre <marcpre@email.com>
2017-10-08 21:42:39 +0200

    fixing pagination

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const cookieParser = require('cookie-parser')
 const fs = require('fs')
 const app = express()
 
+//pagination
+const pageSize = 6
+let start = 0
+let currentPage = 1
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
@@ -18,15 +23,16 @@ app.use(cookieParser())
 
 //Routes
 app.get('/', (req, res) => {
-    
-    const data = JSON.parse(fs.readFileSync('./data/data.json', 'utf8')); 
-    
-    //pagination
+
+    const data = JSON.parse(fs.readFileSync('./data/data.json', 'utf8'));
     const totalRec = Object.keys(data).length
-    const pageSize = 6
     const pageCount = Math.ceil(totalRec / pageSize)
-    const start = 0
-    const currentPage = 1
+    
+    if (typeof req.query.page !== 'undefined') {
+        currentPage = req.query.page
+        console.log(currentPage)
+    }
+    console.log(" req.query.page " + req.query.page)
 
     if (currentPage > 1) {
         start = (currentPage - 1) * pageSize;
@@ -34,9 +40,9 @@ app.get('/', (req, res) => {
 
     console.log("pageCount: " + pageCount + " totalRec: " + totalRec + " start: " + start)
 
-    const postList = data.slice(start, start+pageSize)
-    console.log("postList: " + postList)    
-    
+    const postList = data.slice(start, start + pageSize)
+    console.log("postList: " + postList)
+
     res.render("index", {
         postList,
         totalRec,
@@ -55,4 +61,4 @@ app.listen(port, function () {
     console.log("Listening on " + host + ":" + port)
 })
 
-module.exports = app 
+module.exports = app
